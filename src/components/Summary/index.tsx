e8a27dc7709610d678207cbd268b646db3ebec62
rafaelnajman <rafael.najman@hotmail.com>
2022-06-22 23:51:50 +0300

    added color change based on value in total

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,78 +1,78 @@
-import React, { useContext } from "react";
-
 import { Container } from "./styles";
 
-import IncomeImg  from '../../assets/income.svg'
-import OutcomeImg from '../../assets/outcome.svg'
-import TotalImg from '../../assets/total.svg'
+import IncomeImg from "../../assets/income.svg";
+import OutcomeImg from "../../assets/outcome.svg";
+import TotalImg from "../../assets/total.svg";
 import { useTransactions } from "../../hooks/useTransactions";
 
-export function Summary(){
-    const {transactions} = useTransactions(); // hook que permite trazer informacao para qualquer componente
+export function Summary() {
+  const { transactions } = useTransactions(); // hook que permite trazer informacao para qualquer componente
 
-    // const totalDeposits = transactions.reduce((acc, transaction) => {
-    //     if (transaction.type === 'deposit') {
-    //         return acc + transaction.amount;
-    //     }
+  // const totalDeposits = transactions.reduce((acc, transaction) => {
+  //     if (transaction.type === 'deposit') {
+  //         return acc + transaction.amount;
+  //     }
 
-    //     return acc
-    // }, 0)
-    
-    const summary = transactions.reduce((acc, transaction) => {
-        if(transaction.type === 'deposit'){
-            acc.deposits += transaction.amount
-            acc.total += transaction.amount;
-        }
-        else{
-            acc.withdraw += transaction.amount;
-            acc.total -= transaction.amount;
-        }
-        return acc;
-    },{
-        deposits: 0,
-        withdraw: 0,
-        total: 0,
-    })
+  //     return acc
+  // }, 0)
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraw += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraw: 0,
+      total: 0,
+    }
+  );
 
-    return(
-        <Container>
-            <div>
-                <header>
-                    <p>Income</p>
-                    <img src={ IncomeImg } alt="Income logo" />
-                </header>
-                <strong>
-                {new Intl.NumberFormat('en', {
-                    style: 'currency',
-                    currency: 'USD'
-                    }).format(summary.deposits)}
-                </strong>
-            </div>
-            <div>
-                <header>
-                    <p>outcome</p>
-                    <img src={ OutcomeImg } alt="Income logo" />
-                </header>
-                <strong>- 
-                {new Intl.NumberFormat('en', {
-                    style: 'currency',
-                    currency: 'USD'
-                    }).format(summary.withdraw)}
-                </strong>
-            </div>
-            <div className= "Highlight-background">
-                <header>
-                    <p>Total</p>
-                    <img src={ TotalImg } alt="Income logo" />
-                </header>
-                <strong>{new Intl.NumberFormat('en', {
-                    style: 'currency',
-                    currency: 'USD'
-                    }).format(summary.total)}
-                </strong>
-            </div>
-        </Container>
-    )
-    
-};
+  return (
+    <Container total={summary.total}>
+      <div>
+        <header>
+          <p>Income</p>
+          <img src={IncomeImg} alt="Income logo" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("en", {
+            style: "currency",
+            currency: "USD",
+          }).format(summary.deposits)}
+        </strong>
+      </div>
+      <div>
+        <header>
+          <p>outcome</p>
+          <img src={OutcomeImg} alt="Income logo" />
+        </header>
+        <strong>
+          -
+          {new Intl.NumberFormat("en", {
+            style: "currency",
+            currency: "USD",
+          }).format(summary.withdraw)}
+        </strong>
+      </div>
+      <div className="Highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={TotalImg} alt="Income logo" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("en", {
+            style: "currency",
+            currency: "USD",
+          }).format(summary.total)}
+        </strong>
+      </div>
+    </Container>
+  );
+}
--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,34 +1,41 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3,1fr);
-    gap: 2rem;
-    margin-top: -10rem;
+type Props = {
+  total: number;
+};
 
-    div{
-        background:var(--shape);
-        padding: 1.5rem 2rem;
-        border-radius: 0.25rem;
-        color:var(--text-title);
-        
-        header{
-            display:flex;
-            align-items:center;
-            justify-content:space-between;
-        }
+export const Container = styled.div.attrs((props: Props) => ({
+  total: props.total,
+}))`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -10rem;
 
-        strong{
-            margin-top: 1rem;
-            font-size: 2rem;
-            line-height: 3rem;
-            font-weight:500;
-            display:block;
-        }
+  div {
+    background: var(--shape);
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    color: var(--text-title);
 
-        &.Highlight-background{
-            background: var(--green);
-            color: #fff;
-        }
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     }
+
+    strong {
+      margin-top: 1rem;
+      font-size: 2rem;
+      line-height: 3rem;
+      font-weight: 500;
+      display: block;
+    }
+
+    &.Highlight-background {
+      background: ${(props) =>
+        props.total > 0 ? "var(--green)" : "var(--red)"};
+      color: #fff;
+    }
+  }
 `;
